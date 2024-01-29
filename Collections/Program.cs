f1b23467bad6d4d3baf166eb89061e2b3c4fdc5e
diegorafael <diegorafael_si@msn.com>
2024-01-29 21:16:39 +0000

    commit teste

---
+++ Collections/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Security.Cryptography.X509Certificates;
+using static Collections.Supplier;
+
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var supplier = new Supplier();
+
+            supplier.Talk( x => Console.WriteLine(x.ToUpper()) );
+
+            Console.ReadLine();
+        }
+
+        static void Escritor(string text)
+        {
+            Console.WriteLine($"FROM ESCRITOR: {text}");
+        }
+    }
+}
---
+++ Collections/Supplier.cs
@@ -0,0 +1,19 @@
+﻿namespace Collections
+{
+
+    internal class Supplier
+    {
+        //delegate void Writter(string text);
+
+        
+
+        internal void Talk(Action<string> writter = null)
+        {
+            Console.WriteLine("--- start from Supplier ---");
+            
+            writter?.Invoke("teste supplier");
+            
+            Console.WriteLine("--- end from Supplier ---");
+        }
+    }
+}
